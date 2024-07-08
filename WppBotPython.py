8e99fc0018776f9e27dede15a1c47da6632ac0e0
Eduardo Murillo <57548715+duhmurillo@users.noreply.github.com>
2024-07-07 22:41:49 -0300

    1.0.2 Update Version And New Features (#2)
    * Update version and increment new features
    
    * Increment version
    
    ---------
    
    Co-authored-by: duhmurillo <eduardo.oliveira@agilecontent.com>

--- WppBotPython.py
+++ WppBotPython.py
@@ -10,6 +10,11 @@ import time
 import os
 from datetime import datetime
 
+# Verifica e cria a pasta de logs se não existir
+log_directory = 'logs'
+if not os.path.exists(log_directory):
+    os.makedirs(log_directory)
+
 # Função para ler os números de telefone do arquivo
 def ler_numeros(arquivo):
     with open(arquivo, 'r') as f:
@@ -21,16 +26,22 @@ def enviar_mensagem(numero, mensagem):
     try:
         # Abre a conversa com o número especificado
         driver.get(f'https://web.whatsapp.com/send?phone=55{numero}&text&app_absent=0')
-        time.sleep(10)  # Espera a conversa carregar
-
-        # Encontra a caixa de mensagem e envia a mensagem
-        caixa_mensagem = driver.find_element(By.XPATH, '//div[@contenteditable="true"][@data-tab="10"]')
+        
+        # Espera até que a caixa de mensagem esteja disponível
+        caixa_mensagem = WebDriverWait(driver, 20).until(
+            EC.presence_of_element_located((By.XPATH, '//div[@contenteditable="true"][@data-tab="10"]'))
+        )
+        
+        # Copia e cola a mensagem, espera 1 segundo para garantir que a mensagem foi colada
+        pyperclip.copy(mensagem)
         caixa_mensagem.send_keys(Keys.CONTROL, 'v')
+        time.sleep(1)     
+        
         caixa_mensagem.send_keys(Keys.ENTER)
-        time.sleep(5)  # Espera a mensagem ser enviada
+        time.sleep(3)  # Espera a mensagem ser enviada
 
         # Log de sucesso
-        with open(os.path.join('logs', 'success.log'), 'a', encoding='utf-8') as log_file:
+        with open(os.path.join(log_directory, 'success.log'), 'a', encoding='utf-8') as log_file:
             log_file.write(f"{datetime.now()} - Mensagem enviada para {numero}\n")
 
         print(f"{datetime.now()} - Mensagem enviada com sucesso para o número {numero}")
@@ -38,18 +49,19 @@ def enviar_mensagem(numero, mensagem):
     except Exception as e:
         # Log de erro simplificado
         error_message = f"{datetime.now()} - Erro ao enviar mensagem para {numero}"
-        with open(os.path.join('logs', 'error.log'), 'a', encoding='utf-8') as log_file:
+        with open(os.path.join(log_directory, 'error.log'), 'a', encoding='utf-8') as log_file:
             log_file.write(f"{error_message}\n")
-
         print(error_message)
 
 # Configura o driver do Chrome
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 
 # Mensagem personalizada no terminal
-print("Desenvolvido por @duhmurillo (https://instagram.com/duhmurillo)")
-print("Por favor, escaneie o código QR com o seu aplicativo WhatsApp para iniciar a execução!")
-print("Após escanear, apenas relaxe e deixe com a gente ;)\n")
+print(" ==========================================================================================")
+print("|| Desenvolvido por @duhmurillo (https://instagram.com/duhmurillo)                        ||")
+print("|| Por favor, escaneie o código QR com o seu aplicativo WhatsApp para iniciar a execução! ||")
+print("|| Após escanear, apenas relaxe e deixe com a gente :)                                    ||")
+print(" ==========================================================================================\n")
 
 # Abra o WhatsApp Web para login manual
 driver.get('https://web.whatsapp.com')
@@ -58,11 +70,7 @@ driver.get('https://web.whatsapp.com')
 try:
     WebDriverWait(driver, timeout=600).until(
         EC.presence_of_element_located((By.XPATH, '//div[@id="pane-side"]'))
-    )
-    print("QR code escaneado. Iniciando processo em 5")
-    time.sleep(1)
-    print("QR code escaneado. Iniciando processo em 4")
-    time.sleep(1)
+    )    
     print("QR code escaneado. Iniciando processo em 3")
     time.sleep(1)
     print("QR code escaneado. Iniciando processo em 2")
@@ -86,11 +94,11 @@ try:
     # Loop para enviar mensagens para cada número
     for numero in numeros:
         try:
-            enviar_mensagem(numero, pyperclip.copy(mensagem))            
+            enviar_mensagem(numero, mensagem)
         except Exception as e:
             print(f"Erro ao processar o número {numero}: {e}")
         
-        time.sleep(5)  # Espera um tempo entre mensagens para evitar bloqueios
+        time.sleep(2)  # Espera um tempo entre mensagens para evitar bloqueios
 
     # Calcula o tempo total de execução
     end_time = time.time()
